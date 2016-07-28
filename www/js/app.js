c433ee5f7a6945cd5db82a8e2cbac3fc2a3251bc
michaeljymsgutierrez <michael@vielsoft.com>
2016-07-28 09:43:12 +0800

    change ng-app

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic'])
+var app = angular.module('facebook', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,4 @@ var app = angular.module('starter', ['ionic'])
     }
   });
 })
+
