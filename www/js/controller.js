9c6a93a80c23ea22ee66b24d23fc7c359ef343d0
michaeljymsgutierrez <michael@vielsoft.com>
2016-07-28 13:02:12 +0800

    updated codes

--- www/js/controller.js
+++ www/js/controller.js
@@ -6,4 +6,10 @@ app.controller('fbCtrl',function($scope,$window,$cordovaInAppBrowser){
         $window.open("https://github.com/michaeljymsgutierrez",'_self',"location=no");
     };
     
+    
+    $scope.fbDirect = function(){
+        
+        $window.open("https://m.facebook.com/",'_self',"location=no");  
+    };
+    
 });
