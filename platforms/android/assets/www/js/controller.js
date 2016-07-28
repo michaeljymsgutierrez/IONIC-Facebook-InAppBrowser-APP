72e5801b203db44b76f1ad6ba24a4ae8056571c8
michaeljymsgutierrez <michael@vielsoft.com>
2016-07-28 13:07:00 +0800

    platforms added

---
+++ platforms/android/assets/www/js/controller.js
@@ -0,0 +1,26 @@
+app.controller('fbCtrl',function($scope,$window,$cordovaInAppBrowser,$timeout,$ionicLoading){
+    
+    
+    $scope.gitChael = function(){
+        
+        $window.open("https://github.com/michaeljymsgutierrez",'_self',"location=no");
+    };
+    
+    
+    $scope.fbDirect = function(){
+       $ionicLoading.show({template:"Connecting facebook . . ."});
+       
+       $timeout(function(){
+       
+                        $ionicLoading.hide();
+               
+        },1000);
+        
+
+        $window.open("https://m.facebook.com/",'_self',"location=no");  
+        
+    };
+    
+
+    
+});
