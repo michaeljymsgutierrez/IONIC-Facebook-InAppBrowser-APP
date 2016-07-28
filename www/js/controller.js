f2256bb45b5e7f7f2361cda3b6e2d82522f755a7
michaeljymsgutierrez <michael@vielsoft.com>
2016-07-28 13:13:01 +0800

    update

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,4 +1,4 @@
-app.controller('fbCtrl',function($scope,$window,$cordovaInAppBrowser){
+app.controller('fbCtrl',function($scope,$window,$cordovaInAppBrowser,$timeout,$ionicLoading){
     
     
     $scope.gitChael = function(){
@@ -8,8 +8,19 @@ app.controller('fbCtrl',function($scope,$window,$cordovaInAppBrowser){
     
     
     $scope.fbDirect = function(){
+       $ionicLoading.show({template:"Connecting facebook . . ."});
+       
+       $timeout(function(){
+       
+                        $ionicLoading.hide();
+               
+        },1000);
         
+
         $window.open("https://m.facebook.com/",'_self',"location=no");  
+        
     };
+    
+
     
 });
