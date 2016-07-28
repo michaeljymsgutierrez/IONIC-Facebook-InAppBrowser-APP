1de868f2dc5a193e9720880a111c1c14a593030f
michaeljymsgutierrez <michael@vielsoft.com>
2016-07-28 12:21:19 +0800

    link gitChael()

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,6 +1,9 @@
 app.controller('fbCtrl',function($scope,$window,$cordovaInAppBrowser){
     
     
-
+    $scope.gitChael = function(){
+        
+        $window.open("https://github.com/michaeljymsgutierrez",'_self',"location=no");
+    };
     
 });
